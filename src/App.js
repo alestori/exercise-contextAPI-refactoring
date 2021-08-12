a88c7529d58b46c7dc97fea0ec0a3dc84d4bdb15
Merlone <alestori7@gmail.com>
2021-08-12 10:47:12 -0300

    exercicio 1 :white_check_mark:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Cars from './Cars';
+import carsContext from './Context/carsContext';
 
 function App() {
+  const [cars, setValue] = useState({ red: false, blue: false, yellow: false});
+
+  const moveCar = (car, side) => {
+    setValue({...cars, [car]: side })
+  }
+
+  const contextValue = {
+    cars, moveCar
+  }
+
   return (
-    <Cars />
+    <carsContext.Provider value={contextValue}>
+      <Cars />
+    </carsContext.Provider>
   );
 }
 
